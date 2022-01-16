27307984fa9cf5a591d94bf793c505d256b9c69c
sushma1440 <97818737+sushma1440@users.noreply.github.com>
2022-01-16 20:14:31 +0530

    created grading.c
    this file will grade the system based on the given  number and display on the screen

---
+++ grading.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+  int mark;
+  printf("Enter a number");
+  scanf("%d, &mark);
+if(mark>=85&&mark<=100)
+        {
+          printf("A grade");
+        }
+        else if(mark>=70&&mark<=84)
+        {
+          ptintf("B grade");
+        }
+          else if(mark>=55&&mark<=69)
+          {
+            printf("C grade");
+          }
+        else if(mark>=40&&mark<=54)
+        {
+          printf("D grade");
+        }
+        else if(below 40)
+        {
+          printf("F grade");
+        }
+        return 0;
+        }
+  
+            
+     
+   
